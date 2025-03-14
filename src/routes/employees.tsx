213d7ccdef6fe417c5c77033fab206ac7942ff6b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-03-14 10:18:03 -0400

    add working test

--- src/routes/employees.tsx
+++ src/routes/employees.tsx
@@ -1,64 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import type { Employee } from '../api/employees'
+import EmployeesAPI from '../api/employees'
 
 export const Route = createFileRoute('/employees')({
   component: EmployeesComponent,
-  loader: async () => [
-    {
-      id: 1,
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    {
-      id: 2,
-      firstName: 'Jane',
-      lastName: 'Smith',
-    },
-    {
-      id: 3,
-      firstName: 'Alice',
-      lastName: 'Johnson',
-    },
-    {
-      id: 4,
-      firstName: 'Bob',
-      lastName: 'Brown',
-    },
-    {
-      id: 5,
-      firstName: 'Charlie',
-      lastName: 'Davis',
-    },
-    {
-      id: 6,
-      firstName: 'Diana',
-      lastName: 'Miller',
-    },
-    {
-      id: 7,
-      firstName: 'Eve',
-      lastName: 'Wilson',
-    },
-    {
-      id: 8,
-      firstName: 'Frank',
-      lastName: 'Moore',
-    },
-    {
-      id: 9,
-      firstName: 'Grace',
-      lastName: 'Taylor',
-    },
-    {
-      id: 10,
-      firstName: 'Hank',
-      lastName: 'Anderson',
-    },
-    {
-      id: 11,
-      firstName: 'Ivy',
-      lastName: 'Thomas',
-    },
-  ],
+  loader: EmployeesAPI.get,
 })
 
 function EmployeesComponent() {
@@ -69,7 +15,7 @@ function EmployeesComponent() {
     <div className="p-2">
       <h1 className="text-2xl">Employees</h1>
       <ul className="employee-list">
-        {employees.map((employee) => (
+        {employees.map((employee: Employee) => (
           <li key={employee.id} className="employee">
             {employee.firstName} {employee.lastName}
           </li>
